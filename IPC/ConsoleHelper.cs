632448066b083f6fe4d4f8e0910425fa41803122
Isaac Vrooman <61886068+ihvrooman@users.noreply.github.com>
2022-08-23 12:16:57 -0400

    Finished with named pipes.

--- IPC/ConsoleHelper.cs
+++ IPC/ConsoleHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO.Pipes;
 using System.Linq;
 using System.Text;
@@ -15,8 +16,9 @@ namespace IPC
         private object _consoleLock = new();
         private NamedPipeServerStream _serverPipe;
         private NamedPipeClientStream _clientPipe;
-        private static Mutex _mutex1 = new Mutex(false, "Pipe1");
-        private static Mutex _mutex2 = new Mutex(false, "Pipe2");
+        private StreamReader _reader;
+        private StreamWriter _writer;
+        private bool _disposed = false;
 
         public ConsoleHelper(string consoleName, string pipeName, string serverPipeNameToConnectTo)
         {
@@ -30,15 +32,6 @@ namespace IPC
             Console.WriteLine($"{ConsoleName} starting up...");
             Console.WriteLine("Initializing server pipe...");
             _serverPipe = NamedPipeServerService.GenerateNamedPipeServerStream(ServerPipeName);
-            if (ServerPipeName == "Pipe1")
-            {
-                _mutex1.Close();
-            }
-            else if(ServerPipeName == "Pipe2")
-            {
-                _mutex2.Close();
-            }
-
             Console.WriteLine($"{ServerPipeName} initialized and connected.");
 
             ConnectToServerPipe(ServerPipeNameToConnectTo);
@@ -50,39 +43,60 @@ namespace IPC
             _serverPipe.WaitForConnection();
             Console.WriteLine("Connection established.");
 
-            Console.WriteLine(Environment.NewLine + Environment.NewLine + "Write the message that you wish to send over the pipe and press ENTER to send.");
+            Console.WriteLine(Environment.NewLine + Environment.NewLine + "Write the message that you wish to send over the pipe and press ENTER to send." + Environment.NewLine + Environment.NewLine + "Type CLOSE to exit the application." + Environment.NewLine + Environment.NewLine);
         }
 
         public void BeginWrite()
         {
-            while (true)
+            _writer = new StreamWriter(_serverPipe)
+            {
+                AutoFlush = true
+            };
+
+            while (!_disposed)
             {
                 var s = Console.ReadLine();
-                if (s != null)
+
+                if (!_disposed && s != null)
                 {
                     lock (_consoleLock)
                     {
-                        _serverPipe.Write(s);
+                        _writer.WriteLine(s);
+                    }
+
+                    if (s.ToLower() == "close")
+                    {
+                        Dispose();
                     }
                 }
             }
         }
 
         public void BeginRead()
-        {
+        { 
+            _reader = new StreamReader(_clientPipe);
             Task.Run(() =>
             {
-                var m = _clientPipe.Read();
-
-                if (!string.IsNullOrEmpty(m))
+                while (!_disposed)
                 {
-                    lock (_consoleLock)
-                    {
-                        Console.WriteLine($"Message from {ServerPipeNameToConnectTo}: {m}");
-                    }
-                }
+                    var m = _reader.ReadLine();
 
-                Task.Delay(100).Wait();
+                    if (!string.IsNullOrEmpty(m))
+                    {
+                        if (m.ToLower() == "close")
+                        {
+                            Dispose();
+                            break;
+                        }
+
+                        lock (_consoleLock)
+                        {
+                            Console.WriteLine($"Message from {ServerPipeNameToConnectTo}: {m}");
+                        }
+                    }
+
+                    Task.Delay(100).Wait();
+                }
             });
         }
 
@@ -91,16 +105,26 @@ namespace IPC
             if (serverPipeName != ServerPipeName)
             {
                 Console.WriteLine($"Connecting to server pipe '{serverPipeName}'...");
-                var name = ServerPipeName + "Client";
-                var mutex = ServerPipeNameToConnectTo == "Pipe1" ? _mutex1 : _mutex2;
-                if (!mutex.WaitOne(0, false))
-                {
-                    _clientPipe = NamedPipeServerService.GenerateNamedPipeClientStream(serverPipeName, name);
-                    _clientPipe.Connect();
-                }
-                //mutex.Close();
+                Task.Delay(10000).Wait();
+                _clientPipe = NamedPipeServerService.GenerateNamedPipeClientStream(serverPipeName);
+                _clientPipe.Connect();
                 Console.WriteLine("Connected.");
             }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Shutting down...");
+            _disposed = true;
+            Task.Delay(200).Wait();
+            _reader.Dispose();
+            _writer.Dispose();
+            _clientPipe.Close();
+            _serverPipe.Close();
+            _clientPipe.Dispose();
+            _serverPipe.Dispose();
+            Process.GetCurrentProcess().Close();
+            Console.Write("Press ENTER to close...");
         }
     }
 }
--- IPC/NamedPipeServerService.cs
+++ IPC/NamedPipeServerService.cs
@@ -5,40 +5,14 @@ namespace IPC
 {
     public static class NamedPipeServerService
     {
-        public static Encoding Encoding = Encoding.Unicode;
         public static NamedPipeServerStream GenerateNamedPipeServerStream(string name)
         {
-            return new NamedPipeServerStream(name, PipeDirection.InOut, 10);
+            return new NamedPipeServerStream(name, PipeDirection.Out, 1);
         }
 
-        public static NamedPipeClientStream GenerateNamedPipeClientStream(string serverName, string name)
+        public static NamedPipeClientStream GenerateNamedPipeClientStream(string serverName)
         {
-            return new NamedPipeClientStream(serverName, name);
-        }
-
-        public static void Write(this NamedPipeServerStream namedPipeServerStream, string message)
-        {
-            using (StreamWriter sw = new (namedPipeServerStream))
-            {
-                sw.AutoFlush = true;
-                sw.Write(message);
-            }
-        }
-
-        public static string Read(this NamedPipeClientStream namedPipeClientStream)
-        {
-            using (StreamReader sr = new (namedPipeClientStream))
-            {
-                return sr.ReadToEnd();
-            }
-            //    var s = string.Empty;
-            //var bytes = new byte[500];
-            //while (namedPipeServerStream.Read(bytes, 0, int.MaxValue) > 0)
-            //{
-            //    s += Encoding.GetString(bytes);
-            //}
-
-            //return s;
+            return new NamedPipeClientStream(".", serverName, PipeDirection.In);
         }
     }
 }
